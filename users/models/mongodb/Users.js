0d8683406ffc589a29a788e4a5a1e1f3670dcfd9
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-02-21 15:18:27 -0500

    cs import

--- users/models/mongodb/Users.js
+++ users/models/mongodb/Users.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { EMAIL_VALIDATION, PHONE_VALIDATION } from '../../../helpers/MongoDB/mongooseValidators.js';
 import NameSchema from '../../../helpers/MongoDB/NameSchema.js';
 import ImageSchema from '../../../helpers/MongoDB/ImageSchema.js';
-import AddressSchema from '../../../helpers/MongoDB/addressSchema.js';
+import AddressSchema from '../../../helpers/MongoDB/AddressSchema.js';
 
 const userSchema = new mongoose.Schema({
     name: NameSchema,
