cfd9f93d658c48c60ac5ed28fe1741c4560d4168
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-02-22 18:37:27 -0500

    cors PATCH method

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 const corsMiddleware = cors({
     origin: ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5173", "http://localhost:5174", "https://backend-businessdirectory.onrender.com"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true, 
     preflightContinue: false,
 });
