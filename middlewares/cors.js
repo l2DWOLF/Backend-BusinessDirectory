9e63545466db774458a5cba3ab42868549a8b465
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-06-05 07:39:09 -0400

    cors frontend fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const corsMiddleware = cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5173", "http://localhost:5174", "https://backend-businessdirectory.onrender.com"],
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5173", "http://localhost:5174", "https://ie-business-directory.onrender.com"],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true, 
     preflightContinue: false,
